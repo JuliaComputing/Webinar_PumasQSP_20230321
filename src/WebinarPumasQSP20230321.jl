84ed8a7854bacba274aa1ef2d474190ccd3328e9
Paul Lang <47160416+paulflang@users.noreply.github.com>
2023-03-14 20:56:18 +0100

    add barebones sbml simulation

--- src/WebinarPumasQSP20230321.jl
+++ src/WebinarPumasQSP20230321.jl
@@ -1,5 +1,28 @@
 module WebinarPumasQSP20230321
 
-greet() = print("Hello World!")
+using PumasQSP
+using SBMLToolkit
+
+model_dir = joinpath(@__DIR__, "..", "Erdem_PlOSComputBiol2021")
+sbmlfile = joinpath(model_dir, "model_sbml.xml")
+
+## Import BioNetGen model
+# add `writeSBML()` to the bottom of your BNGL file and run it
+
+## Import SBML
+sys = readSBML(sbmlfile, ODESystemImporter())
+
+using ModelingToolkit
+sys = structural_simplify(sys)
+
+using OrdinaryDiffEq
+prob = ODEProblem(sys, [], (0, 1700))
+sol = solve(prob)
+
+## Import Data
+trial1 = Trial(nothing, ssys)
+
+## Import the whole problem as a PEtab model
+
 
 end # module WebinarPumasQSP20230321
